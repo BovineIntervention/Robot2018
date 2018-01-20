24c85c6b2f215336c7b6408923cda804dd02544b
BovineIntervention <team686@gmail.com>
2018-01-20 11:37:21 -0500

    Initial commit

--- src/org/usfirst/frc/team686/robot/loop/ElevatorLoop.java
+++ src/org/usfirst/frc/team686/robot/loop/ElevatorLoop.java
@@ -1,28 +1,62 @@
 package org.usfirst.frc.team686.robot.loop;
 
-import org.usfirst.frc.team686.robot.command_status.ElevatorCommand;
+import org.usfirst.frc.team686.robot.Constants;
 
-public class ElevatorLoop implements Loop {
-
-	@Override
-	public void onStart() {
-		// TODO Auto-generated method stub
+public class ElevatorLoop {
+	
+	public enum ElevatorMode { UNINITIALIZED, ZEROING, RUNNING, ESTOPPED; }
+	private static double goal;
+	private ElevatorMode mode = ElevatorMode.UNINITIALIZED;
+	private static double offset = 0.0;
+	public static double lastError = 0.0;
+	private static double error_;
+	private static double filteredGoal;
+	
+	
+	public double Update(double encoder, boolean limitTriggered, boolean enabled){
+		double position = encoder - offset;
 		
-	}
-
-	@Override
-	public void onLoop() {
-		// TODO Auto-generated method stub
+		switch(mode)
+		{
+		case UNINITIALIZED:
+			if(enabled){
+				mode = ElevatorMode.ZEROING;
+				filteredGoal = position;
+			}
+			break;
+		case ZEROING:
+			filteredGoal  = filteredGoal - (Constants.kElevatorDt * Constants.kElevatorZeroingVelocity);
+			if(limitTriggered){
+				mode = ElevatorMode.RUNNING;
+				offset = -encoder;
+				position = 0.0;
+			}
+			if(!enabled){
+				mode = ElevatorMode.UNINITIALIZED;
+			}
+			break;
+		case RUNNING:
+			filteredGoal = goal;
+			break;
+		case ESTOPPED:
+			break;
+		default:
+			mode = ElevatorMode.UNINITIALIZED;
+		}
 		
-	}
-
-	@Override
-	public void onStop() {
-		// TODO Auto-generated method stub
-		
+		double Kp = 100.0;
+		double Kv = 0.1;
+		double error = filteredGoal - position;
+		double vel = (error - lastError) / Constants.kElevatorDt;
+		lastError = error;
+		error_ = error;
+		double voltage = Kp * error + Kv * vel;
+		return Math.min(Constants.kElevatorMaxVoltage, Math.max(-Constants.kElevatorMaxVoltage, voltage));
 	}
 	
-	private void setControlMode(ElevatorCommand newCmd){
-		
-	}
+	public void setGoal(double goal_){ goal = goal_; }
+	public double getGoal () { return goal; } 
+	
+	public double getFilteredGoal() { return filteredGoal; }
+
 }
--- test/ElevatorTest.java
+++ test/ElevatorTest.java
@@ -1,52 +1,64 @@
 
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
 public class ElevatorTest {
 
+	public ElevatorTest(){
+		
+	}
+
 	
-    double kVoltage = 12.0;
-    
-    double kStallTorque = 2.402; //Stall Torque in N*m
-    double kStallCurrent = 126.145; //Stall Current in Amps
-    double kFreeSpeed = 5015.562; //Free Speed in RPM
-    double kFreeCurrent = 1.170; //Free Current in Amps
-    double kMass = 20.0; //Mass of the Elevator
-    
-    double kNumMotors = 2.0; //Number of motors
-    double kResistance = kVoltage/kStallCurrent; //Resistance of motor
-    double Kv = ((kFreeSpeed / 60) * (2.0 * Math.PI)) / 
-    							(kVoltage - kResistance * kFreeCurrent); //Motor velocity constant (kFreeSpeed converted to rad/s)
-    double Kt = (kNumMotors * kStallTorque) / kStallCurrent; //Torque constant
-    double kG = 5; //Gear ratio
-    double kr = 17 * 0.25 * 0.0254 / Math.PI / 2.0; //radius of pulley
-    
-    double kDt = 0.010; //Control loop time step
-	
-    public double getAcceleration (double voltage){
-		return(-Kt * kG * kG / (Kv * kResistance * kr * kr * kMass) * velocity_ + kG * Kt / 
-				(kResistance * kr * kMass) * voltage);
+    public static double getAcceleration (double voltage){
+		return(-Constants.KElevatorTorque * Constants.kElevatorGearRatio * Constants.kElevatorGearRatio
+				/ (Constants.KMotorVelocity * Constants.kElevatorResistance * Constants.kElevatorPulleyRadius * Constants.kElevatorPulleyRadius * Constants.kElevatorMass) * 
+				velocity_ + Constants.kElevatorGearRatio * Constants.KElevatorTorque / 
+				(Constants.kElevatorResistance * Constants.kElevatorPulleyRadius * Constants.kElevatorMass) * voltage);
 	}
     
-    double position_ = 0.0;
-    double velocity_ = 0;
-    public boolean limit_triggered(){ return position_ > -0.04 && position_ < 0.0; }
+    public static double position_ = 0.1;
+    public static double velocity_ = 0;
+    public static double offset = 0.1;
+    
+    public static double getEncoder() { return position_ + offset; }
+    public static boolean getLimitTriggered(){ return position_ > -0.04 && position_ < 0.0; }
 
-    public void SimulateTime(double time){
+    public static void SimulateTime(double voltage, double time){
+    	assertTrue("voltage level is out of range: " + voltage, -Constants.kElevatorVoltage <= voltage && voltage <= Constants.kElevatorVoltage);
     	double kSimTime = 0.0001;
     	double current_time = 0.0;
     	while (current_time < time){
     		position_ += kSimTime * velocity_;
-    		velocity_ += kSimTime * getAcceleration(kVoltage);
-    		current_time += kSimTime;	
+    		velocity_ += kSimTime * getAcceleration(voltage);
+    		current_time += kSimTime;
+    		if(getLimitTriggered()){ assertTrue("VELOCITY" + velocity_, velocity_ > -0.05); }
     	}
     	
     }
-	
-    @Test
-	public void test() {
-		ElevatorCommand elevator;
+    
+	@Test
+	public void test() throws FileNotFoundException, UnsupportedEncodingException {
+    	ElevatorLoop elevator = new ElevatorLoop();
+    	elevator.setGoal(0.5);
+    	String file = "C:\\Users\\susan\\Documents\\FIRST 2018\\testing\\ElevatorTest.csv";
+    	PrintWriter writer = new PrintWriter(file, "UTF-8");//("C:\\Users\\team686\\Desktop\\Documents\\Robot2018Testing\\ElevatorTest.csv", "UTF-8");
+    	writer.println("TIME,VOLTAGE,POSITION,VELOCITY,GOAL,ERROR");
 		double current_time = 0.0;
-		while (current_time < 1.0) {
-			double voltage = elevator.getVoltage()
+		while (current_time < 10.0) {
+			double voltage = elevator.Update(getEncoder() , getLimitTriggered(), true);
+			assertTrue("voltage level is out of range: " + voltage, -Constants.kElevatorVoltage <= voltage && voltage <= Constants.kElevatorVoltage);
+			SimulateTime(voltage, Constants.kElevatorDt);
+			current_time += Constants.kElevatorDt;
+			writer.printf("%f,%f,%f,%f,%f,%f%n", current_time, voltage, position_, velocity_, elevator.getFilteredGoal(), elevator.lastError);
 		}
+		writer.close();
+		assertEquals(0.5, position_, 0.01);
 	}
+	
 }
-
